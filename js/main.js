3b49e6c3b9dead36b2cdd8078d5a88af0257d99a
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-11-22 09:57:19 +0100

    add 3 day 3

--- js/main.js
+++ js/main.js
@@ -14,27 +14,22 @@ const app = new Vue({
     methods: {
         generateEmail() {
 
+            for(let i = 0; i < 10 ; i++) {
 
-            axios.get(this.apiEmailURL)
+                axios.get(this.apiEmailURL)
 
-            .then(response => {
-                // handle success
-                const email= '';
-                for(let i = 0; i < 10 ; i++) {
-                    
-                    const email = response.data.response
-                    console.log(email);
-                    
-                    this.EmailRandom.push(email)
-                }
-                // this.EmailRandom = response.data.response
-                // console.log(response.data.response)
-            })
-
-            .catch(error => {
-            // handle error
-            console.log(error);
-            });
+                .then(response => {
+                    // handle success
+                    this.EmailRandom.push(response.data.response)
+                    console.log(response.data.response)
+                })
+    
+                .catch(error => {
+                // handle error
+                console.log(error);
+                });
+            }
+            
         },
     },
 });
